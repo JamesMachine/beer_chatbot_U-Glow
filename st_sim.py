7485e26396594c939f60c12fefae34070ddd9c08
JamesMachine <104810126+JamesMachine@users.noreply.github.com>
2024-03-04 05:52:25 +0100

    Update st_sim.py

--- st_sim.py
+++ st_sim.py
@@ -1,10 +1,11 @@
+import numpy as np
+import pandas as pd
 import openai
 import streamlit as st
 
 from sentence_transformers import SentenceTransformer
-import numpy as np
-import pandas as pd
 
+# load key
 openai.api_key = st.secrets['OAK']
 
 # prepare transformer
